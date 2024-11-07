cf5d51aeb0f5f4d459974ea69ceff3614becd663
rabiarz <rabiarezaie21@gmail.com>
2024-11-07 17:04:46 +0900

    fix: sublist function

--- sublist.scm
+++ sublist.scm
@@ -1,13 +1,17 @@
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
 (define sublist
-(lambda (n m lat)
-  (cond
-    ((or (null? lat) (= m 0)) '()) 
-    (( = n 1 )                   
-     (cons (car lat) (sublist n (- m 1) (cdr lat))))
-    (else
-     (sublist (- n 1) (- m 1) (cdr lat))))))
+  (lambda (n m lat)
+    (cond
+      ((= m 0) '()) 
+      (( = n 1 )                   
+        (cons (car lat) (sublist n (sub1 m) (cdr lat))))
+          (else
+            (sublist (sub1 n) (sub1 m) (cdr lat))))))
 
 
 (print (sublist `0 `3 `(a b c d e f)))  
 (print (sublist `1 `2 `(a b c d e f)))  
-(print (sublist `3 `5 `(a b c d e f)))  
+(print (sublist `4 `6 `(a b c d e f)))
